7ab9902ac489caf07294c40fa70790d0df42a419
zerbroesler <zerbroesler@gmx.de>
2020-05-07 20:47:12 +0200

    Reading Works. but raw

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,73 @@
 #include <Arduino.h>
-// (c) Michael Schoeffler 2017, http://www.mschoeffler.de
-#include "Wire.h" // This library allows you to communicate with I2C devices.
-const int MPU_ADDR = 0x68; // I2C address of the MPU-6050. If AD0 pin is set to HIGH, the I2C address will be 0x69.
-int16_t accelerometer_x, accelerometer_y, accelerometer_z; // variables for accelerometer raw data
-int16_t gyro_x, gyro_y, gyro_z; // variables for gyro raw data
-int16_t temperature; // variables for temperature data
-char tmp_str[7]; // temporary variable used in convert function
-char* convert_int16_to_str(int16_t i) { // converts int16 to string. Moreover, resulting strings will have the same length in the debug monitor.
-  sprintf(tmp_str, "%6d", i);
-  return tmp_str;
-}
-void setup() {
-  Serial.begin(9600);
+/**
+ * Reading raw data from the GY-521 sensor module (MPU-6050)
+ *
+ * (c) 2018 Christian Grieger
+ * GNU GENERAL PUBLIC LICENSE
+ */
+
+#include <Wire.h>
+
+#define MPU6050_ADRESS 0x68
+int16_t accX, accY, accZ, gyrX, gyrY, gyrZ, tVal;
+double temperature = 0.0;
+
+void setup()
+{
   Wire.begin();
-  Wire.beginTransmission(MPU_ADDR); // Begins a transmission to the I2C slave (GY-521 board)
+  Wire.beginTransmission(MPU6050_ADRESS); // Begins a transmission to the I2C slave (GY-521 board)
   Wire.write(0x6B); // PWR_MGMT_1 register
   Wire.write(0); // set to zero (wakes up the MPU-6050)
   Wire.endTransmission(true);
+
+  Serial.begin(9600);
+  Serial.flush();
 }
-void loop() {
-  Wire.beginTransmission(MPU_ADDR);
+void loop()
+{
+  Wire.beginTransmission(MPU6050_ADRESS);
   Wire.write(0x3B); // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
   Wire.endTransmission(false); // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
-  Wire.requestFrom(MPU_ADDR, 7*2, true); // request a total of 7*2=14 registers
-  
+  Wire.requestFrom(MPU6050_ADRESS, 7*2, true); // request a total of 7*2=14 registers
+
   // "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
-  accelerometer_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
-  accelerometer_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
-  accelerometer_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
-  temperature = Wire.read()<<8 | Wire.read(); // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
-  gyro_x = Wire.read()<<8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
-  gyro_y = Wire.read()<<8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
-  gyro_z = Wire.read()<<8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
-  
-  // print out data
-  Serial.print("aX = "); Serial.print(convert_int16_to_str(accelerometer_x));
-  Serial.print(" | aY = "); Serial.print(convert_int16_to_str(accelerometer_y));
-  Serial.print(" | aZ = "); Serial.print(convert_int16_to_str(accelerometer_z));
-  // the following equation was taken from the documentation [MPU-6000/MPU-6050 Register Map and Description, p.30]
-  Serial.print(" | tmp = "); Serial.print(temperature/340.00+36.53);
-  Serial.print(" | gX = "); Serial.print(convert_int16_to_str(gyro_x));
-  Serial.print(" | gY = "); Serial.print(convert_int16_to_str(gyro_y));
-  Serial.print(" | gZ = "); Serial.print(convert_int16_to_str(gyro_z));
+  accX = Wire.read()<<8 | Wire.read(); // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
+  accY = Wire.read()<<8 | Wire.read(); // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
+  accZ = Wire.read()<<8 | Wire.read(); // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
+  tVal = Wire.read()<<8 | Wire.read(); // reading registers: 0x41 (TEMP_OUT_H) and 0x42 (TEMP_OUT_L)
+  gyrX = Wire.read()<<8 | Wire.read(); // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
+  gyrY = Wire.read()<<8 | Wire.read(); // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
+  gyrZ = Wire.read()<<8 | Wire.read(); // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
+
+  Serial.print("aX = ");    Serial.print(accX);
+  Serial.print(" | aY = "); Serial.print(accY);
+  Serial.print(" | aZ = "); Serial.print(accZ);
+  Serial.print(" | gX = "); Serial.print(gyrX);
+  Serial.print(" | gY = "); Serial.print(gyrY);
+  Serial.print(" | gZ = "); Serial.print(gyrZ);
+
+  /**
+   * The following parameters are taken from the documentation [MPU-6000/MPU-6050 Product Specification, p.14]:
+   *
+   * Temperature sensor is -40°C to +85°C (signed integer)
+   * 340 per °C
+   * Offset = -512 at 35°C
+   * At 0°C: -512 - (340 * 35) = -12412
+   */
+  temperature = (tVal + 12412.0) / 340.0;
+  Serial.print(" | T = ");  Serial.print((int16_t)temperature); Serial.print("°C");
+
   Serial.println();
-  
-  // delay
-  delay(1000);
+  delay(500);
+}
+
+/**
+ * Converts int16 to string.
+ * Moreover, resulting strings will have the same length
+ */
+char* toStr(int16_t value)
+{
+  char result[7];
+  sprintf(result, "%6d", value);
+  return result;
 }
